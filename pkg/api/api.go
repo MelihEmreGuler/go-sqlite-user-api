22e9d29d5c70f0018823c84124fabb3a609d65ad
MelihEmreGuler <melihemreguler@hotmail.com>
2023-04-08 11:31:38 +0300

    Update User API with index and CRUD functionality

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/MelihEmreGuler/go-sqlite-user-api/pkg/database"
 	"github.com/gorilla/mux"
 )
 
@@ -29,16 +31,21 @@ func Api() {
 	fmt.Println("api")
 
 	dataGet()
-	
+
 	// Create a new router using the gorilla/mux package
 	r := mux.NewRouter()
 
 	// Define routes for various endpoints
-	r.HandleFunc("/", index)                           // root endpoint
-	r.HandleFunc("/post", post)                        // post endpoint
-	r.HandleFunc("/post/{category}/{id}", post)         // dynamic post endpoint with parameters
-	r.HandleFunc("/product", product).Methods("GET")   // product endpoint that only accepts GET requests
+	r.HandleFunc("/", index)                         // root endpoint
+	r.HandleFunc("/post", post)                      // post endpoint
+	r.HandleFunc("/post/{category}/{id}", post)      // dynamic post endpoint with parameters
+	r.HandleFunc("/product", product).Methods("GET") // product endpoint that only accepts GET requests
 	//r.HandleFunc("/product", addProduct).Methods("POST") // product endpoint that only accepts POST requests
+
+	r.HandleFunc("/api_get_user", api_get_user)
+	r.HandleFunc("/api_add_user", api_add_user)
+	r.HandleFunc("/api_update_user", api_update_user)
+	r.HandleFunc("/api_delete_user", api_delete_user)
 
 	// Start the HTTP server on port 8080 using the router
 	http.ListenAndServe(":8080", r)
@@ -46,7 +53,15 @@ func Api() {
 
 // Handler function for the root endpoint
 func index(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("index page"))
+	// Write a response message containing information about the API and its usage
+	fmt.Fprintln(w, "Welcome to the User API")
+	fmt.Fprintln(w, "This API allows you to perform CRUD (Create, Read, Update, Delete) operations on users")
+
+	// Provide examples of how to use the API
+	fmt.Fprintln(w, "To get a list of all users, make a GET request to /api_get_user")
+	fmt.Fprintln(w, "To add a new user, make a POST request to /api_add_user with userName and userPassword parameters")
+	fmt.Fprintln(w, "To update an existing user, make a POST request to /api_update_user with userID, userName, and userPassword parameters")
+	fmt.Fprintln(w, "To delete a user, make a POST request to /api_delete_user with userID parameter")
 }
 
 // Handler function for the post endpoints
@@ -84,4 +99,66 @@ func product(w http.ResponseWriter, r *http.Request) {
 	// Write the JSON-encoded response to the client
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(j) //fmt.Fprintf(w, string(j))
+}
+
+func api_get_user(w http.ResponseWriter, r *http.Request) {
+	// Print the matching URL to the console
+	fmt.Println(r.URL.Path)
+
+	// Set the content type of the response to JSON
+	w.Header().Set("content-Type", "application/json")
+
+	// Marshal the UserList slice from the database package into JSON format
+	j, err := json.Marshal(database.UserList)
+	if err != nil {
+		panic(err)
+	}
+
+	// Write the JSON-encoded response to the client
+	fmt.Fprintln(w, string(j))
+}
+
+func api_add_user(w http.ResponseWriter, r *http.Request) {
+	// Print the matching URL to the console
+	fmt.Println(r.URL.Path)
+
+	// Call the AddUser function from the database package with the values from the request parameters
+	database.AddUser(r.FormValue("userName"), r.FormValue("userPassword"))
+
+	// Write a response message containing the added user information
+	fmt.Fprintln(w, "userName:", r.FormValue("userName"), "userPassword:", r.FormValue("userPassword"))
+}
+
+func api_update_user(w http.ResponseWriter, r *http.Request) {
+	// Print the matching URL to the console
+	fmt.Println(r.URL.Path)
+
+	// Parse the userID parameter from the request into an integer
+	id, err := strconv.ParseInt(r.FormValue("userID"), 10, 32)
+	if err != nil {
+		panic(err)
+	}
+
+	// Call the UpdateUser function from the database package with the values from the request parameters
+	database.UpdateUser(int(id), r.FormValue("userName"), r.FormValue("userPassword"))
+
+	// Write a response message containing the updated user information
+	fmt.Fprintln(w, "UserID:", id, "userName:", r.FormValue("userName"), "userPassword:", r.FormValue("userPassword"))
+}
+
+func api_delete_user(w http.ResponseWriter, r *http.Request) {
+	// Print the matching URL to the console
+	fmt.Println(r.URL.Path)
+
+	// Parse the userID parameter from the request into an integer
+	id, err := strconv.ParseInt(r.FormValue("userID"), 10, 32)
+	if err != nil {
+		panic(err)
+	}
+
+	// Call the DeleteUser function from the database package with the parsed userID parameter
+	database.DeleteUser(int(id))
+
+	// Write a response message containing the deleted user information
+	fmt.Fprintln(w, "UserID:", id)
 }
